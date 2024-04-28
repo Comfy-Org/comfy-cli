607172fccb6c2774565e0dcf3826b67c47aca91b
James Kwon <96548424+hongil0316@users.noreply.github.com>
2024-04-28 14:10:49 -0400

    Implement logics to download/remove/list models + ui methods for consistency

---
+++ comfy_cli/ui.py
@@ -0,0 +1,77 @@
+import questionary
+from typing import List, Tuple
+
+from rich.table import Table
+from rich.console import Console
+from rich.progress import Progress
+
+console = Console()
+
+
+def show_progress(iterable, total, description="Downloading..."):
+  """
+  Display progress bar for iterable processes, especially useful for file downloads.
+  Each item in the iterable should be a chunk of data, and the progress bar will advance
+  by the size of each chunk.
+
+  Args:
+      iterable (Iterable[bytes]): An iterable that yields chunks of data.
+      total (int): The total size of the data (e.g., total number of bytes) to be downloaded.
+      description (str): Description text for the progress bar.
+
+  Yields:
+      bytes: Chunks of data as they are processed.
+  """
+  with Progress() as progress:
+    task = progress.add_task(description, total=total)
+    for chunk in iterable:
+      yield chunk
+      progress.update(task, advance=len(chunk))
+
+
+def prompt_multi_select(prompt: str, choices: List[str]) -> List[str]:
+  """
+  Prompts the user to select multiple items from a list of choices.
+
+  Args:
+      prompt (str): The message to display to the user.
+      choices (List[str]): A list of choices from which the user can select.
+
+  Returns:
+      List[str]: A list of the selected items.
+  """
+  selections = questionary.checkbox(prompt, choices=choices).ask()  # returns list of selected items
+  return selections if selections else []
+
+
+def confirm_action(prompt: str) -> bool:
+  """
+  Prompts the user for confirmation before proceeding with an action.
+
+  Args:
+      prompt (str): The confirmation message to display to the user.
+
+  Returns:
+      bool: True if the user confirms, False otherwise.
+  """
+  return questionary.confirm(prompt).ask()  # returns True if confirmed, otherwise False
+
+
+def display_table(data: List[Tuple], column_names: List[str], title: str = "") -> None:
+  """
+  Displays a list of tuples in a table format using Rich.
+
+  Args:
+      data (List[Tuple]): A list of tuples, where each tuple represents a row.
+      column_names (List[str]): A list of column names for the table.
+      title (str): The title of the table.
+  """
+  table = Table(title=title)
+
+  for name in column_names:
+    table.add_column(name, overflow="fold")
+
+  for row in data:
+    table.add_row(*[str(item) for item in row])
+
+  console.print(table)
