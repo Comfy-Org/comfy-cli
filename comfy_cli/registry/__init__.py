86ee29b6407afc2be216442a219d5c8e63222720
James Kwon <96548424+hongil0316@users.noreply.github.com>
2024-05-09 22:02:09 -0400

    fmt

--- comfy_cli/registry/__init__.py
+++ comfy_cli/registry/__init__.py
@@ -5,12 +5,12 @@ from .types import PyProjectConfig, PublishNodeVersionResponse, NodeVersion
 from .zip import zip_files
 
 __all__ = [
-  "RegistryAPI",
-  "extract_node_configuration",
-  "PyProjectConfig",
-  "PublishNodeVersionResponse",
-  "NodeVersion",
-  "zip_files",
-  "upload_file_to_signed_url",
-  "initialize_project_config",
+    "RegistryAPI",
+    "extract_node_configuration",
+    "PyProjectConfig",
+    "PublishNodeVersionResponse",
+    "NodeVersion",
+    "zip_files",
+    "upload_file_to_signed_url",
+    "initialize_project_config",
 ]
