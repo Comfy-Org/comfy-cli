ae972dd3934427e42312825762719e967b473ae4
Robin Huang <robin.j.huang@gmail.com>
2024-07-10 11:02:08 -0700

    Remove here test.

--- comfy_cli/test_cmdline.py
+++ comfy_cli/test_cmdline.py
@@ -5,17 +5,6 @@ from .cmdline import app
 runner = CliRunner()
 
 
-def test_install_here():
-    result = runner.invoke(
-        app,
-        ["--here", "--skip-prompt", "install", "--cpu"],
-    )
-    print("Stdout:")
-    print(result.stdout)
-    assert result.exit_code == 0
-    assert "ComfyUI is installed at" in result.stdout
-
-
 def test_display_version():
     result = runner.invoke(
         app,
