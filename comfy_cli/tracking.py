1a3778d101e35157c3f8c0dfe74e4e45ef61323b
Jason L Weirather <jason-weirather@users.noreply.github.com>
2024-09-17 19:27:43 -0400

    specify we are pulling a boolean

--- comfy_cli/tracking.py
+++ comfy_cli/tracking.py
@@ -45,7 +45,7 @@ def track_event(event_name: str, properties: any = None):
     if properties is None:
         properties = {}
     logging.debug(f"tracking event called with event_name: {event_name} and properties: {properties}")
-    enable_tracking = config_manager.get(constants.CONFIG_KEY_ENABLE_TRACKING)
+    enable_tracking = config_manager.get(constants.CONFIG_KEY_ENABLE_TRACKING, type_cast=bool)
     if not enable_tracking:
         return
 
