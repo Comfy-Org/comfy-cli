1f7407f5102b065fa9883f2da4b42dca1e59d898
telamonian <telamonian@users.noreply.github.com>
2024-09-19 15:36:36 +0900

    added unittest that covers `Find_Req_Files` and related code in `DependencyCompiler.__init__`

--- tests/uv/test_uv.py
+++ tests/uv/test_uv.py
@@ -8,7 +8,8 @@ from comfy_cli.constants import GPU_OPTION
 from comfy_cli.uv import DependencyCompiler
 
 hereDir = Path(__file__).parent.resolve()
-reqsDir = hereDir / "mock_requirements"
+mockComfyDir = hereDir / "mock_comfy"
+mockReqsDir = hereDir / "mock_requirements"
 
 # set up a temp dir to write files to
 testsDir = hereDir.parent.resolve()
@@ -27,19 +28,40 @@ def mock_prompt_select(monkeypatch):
     monkeypatch.setattr(ui, "prompt_select", _mock_prompt_select)
 
 
+def test_find_req_files():
+    mockNodesDir = mockComfyDir / "custom_nodes"
+
+    knownReqFilesCore = [mockComfyDir / "pyproject.toml"]
+    knownReqFilesExt = sorted(
+        [
+            mockNodesDir / "x" / "requirements.txt",
+            mockNodesDir / "y" / "setup.cfg",
+            mockNodesDir / "z" / "setup.py",
+        ]
+    )
+
+    depComp = DependencyCompiler(cwd=mockComfyDir)
+
+    testReqFilesCore = depComp.reqFilesCore
+    testReqFilesExt = sorted(depComp.reqFilesExt)
+
+    assert knownReqFilesCore == testReqFilesCore
+    assert knownReqFilesExt == testReqFilesExt
+
+
 def test_compile(mock_prompt_select):
     depComp = DependencyCompiler(
         cwd=temp,
         gpu=GPU_OPTION.AMD,
         outDir=temp,
-        reqFilesCore=[reqsDir / "core_reqs.txt"],
-        reqFilesExt=[reqsDir / "x_reqs.txt", reqsDir / "y_reqs.txt"],
+        reqFilesCore=[mockReqsDir / "core_reqs.txt"],
+        reqFilesExt=[mockReqsDir / "x_reqs.txt", mockReqsDir / "y_reqs.txt"],
     )
 
     depComp.make_override()
     depComp.compile_core_plus_ext()
 
-    with open(reqsDir / "requirements.compiled", "r") as known, open(temp / "requirements.compiled", "r") as test:
+    with open(mockReqsDir / "requirements.compiled", "r") as known, open(temp / "requirements.compiled", "r") as test:
         # compare all non-commented lines in generated file vs reference file
         knownLines, testLines = [
             [line for line in known.readlines() if not line.strip().startswith("#")],
