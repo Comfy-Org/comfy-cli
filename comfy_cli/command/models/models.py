f2fff4809d49718d84295dc9ebef9e592fe7f64d
Arcitec <38923130+Arcitec@users.noreply.github.com>
2025-07-26 06:16:06 +0200

    fix: Always use "Content-Type: application/json" when contacting CivitAI
    The previous algorithm only applied the correct API Content-Type (JSON) when a token was available.
    
    We now always apply the Content-Type, to ensure that we always get the expected responses.

--- comfy_cli/command/models/models.py
+++ comfy_cli/command/models/models.py
@@ -217,11 +217,12 @@ def download(
     is_civitai_model_url, is_civitai_api_url, model_id, version_id = check_civitai_url(url)
     is_huggingface_url, repo_id, hf_filename, hf_folder_name, hf_branch_name = check_huggingface_url(url)
 
-    if (is_civitai_model_url or is_civitai_api_url) and civitai_api_token is not None:
+    if is_civitai_model_url or is_civitai_api_url:
         headers = {
             "Content-Type": "application/json",
-            "Authorization": f"Bearer {civitai_api_token}",
         }
+        if civitai_api_token is not None:
+            headers["Authorization"] = f"Bearer {civitai_api_token}"
 
     if is_civitai_model_url:
         local_filename, url, model_type, basemodel = request_civitai_model_api(model_id, version_id, headers)
